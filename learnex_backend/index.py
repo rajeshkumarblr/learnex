5cbb5383a509c221e329857b5f1a3c4eed08ffd5
truesaiyan <ayushsaluja@microsoft.com>
2024-02-02 16:48:18 +0530

    adding route name

--- learnex_backend/index.py
+++ learnex_backend/index.py
@@ -8,10 +8,10 @@ api = Api(app, version='1.0', title='Learnex App', description='A comprehensive 
 home_ns = Namespace('home', description='Home Page operations')
 
 # Decorate the class with the route
-@home_ns.route('/')  # This will handle requests to '/home/'
+@home_ns.route('/home')  # This will handle requests to '/home'
 class Home(Resource):
     def get(self):
-        # This method will be called for GET requests to '/home/'
+        # This method will be called for GET requests to '/home'
         return 'This is the home page'
 
 # Add the namespace to the API
