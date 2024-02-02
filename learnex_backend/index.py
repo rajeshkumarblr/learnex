dcfc0d264faff7e5f1aa17916df39fcc709545c7
truesaiyan <ayushsaluja@microsoft.com>
2024-02-02 16:46:57 +0530

    sample swagger application

--- learnex_backend/index.py
+++ learnex_backend/index.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_restx import Api, Resource, Namespace
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Learnex App', description='A comprehensive description of your API')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Define the namespace
+home_ns = Namespace('home', description='Home Page operations')
+
+# Decorate the class with the route
+@home_ns.route('/')  # This will handle requests to '/home/'
+class Home(Resource):
+    def get(self):
+        # This method will be called for GET requests to '/home/'
+        return 'This is the home page'
+
+# Add the namespace to the API
+api.add_namespace(home_ns)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
